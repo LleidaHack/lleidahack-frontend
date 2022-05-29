a1a0a67815d11ebf5eafa2d45e401378fe3579d9
tonlls <tonlls1999@gmail.com>
2022-05-29 13:00:05 +0200

    add api calls

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,10 +1,66 @@
 export async function getAllUsers() {
     try{
         const response = await fetch('https://backend.lleidahack.dev/users');
-        console.log('response  ', response)
+        // console.log('response  ', response)
         return await response.json();
     }catch(error) {
         return [];
-    }
-    
+    }   
+}
+
+export async function getUserById(id) {
+    try{
+        const response = await fetch(`https://backend.lleidahack.dev/users/${id}`);
+        // console.log('response  ', response)
+        return await response.json();
+    }catch(error) {
+        return [];
+    }   
+}
+
+export async function addUser(user) {
+    try{
+        const response = await fetch('https://backend.lleidahack.dev/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        // console.log('response  ', response)
+        return await response.json();
+    }catch(error) {
+        return [];
+    }   
+}
+
+export async function updateUser(user) {
+    try{
+        const response = await fetch(`https://backend.lleidahack.dev/users/${user.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        // console.log('response  ', response)
+        return await response.json();
+    }catch(error) {
+        return [];
+    }   
+}
+
+export async function deleteUser(id) {
+    try{
+        const response = await fetch(`https://backend.lleidahack.dev/users/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        // console.log('response  ', response)
+        return await response.json();
+    }catch(error) {
+        return [];
+    }   
 }
