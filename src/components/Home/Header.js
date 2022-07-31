18a99f1562b1a9be1bb3a0993fbdc3c345cc6f3e
tonlls <tonlls1999@gmail.com>
2022-07-31 23:27:59 +0200

    new structure

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Header( {items} ) {
     return <div>
         {items.map((item) => (
-            <span onclick="location.href='{item.url}';">{item.name}</li>)
+            <span onclick="location.href='{item.url}';">{item.name}</span>)
         )}
         </div>;
 }
