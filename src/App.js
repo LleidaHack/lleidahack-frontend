7e4299b51408b15016a9c285ff6e7e0eb016c83a
Big-Lolo <lotfibouak@gmail.com>
2022-08-05 11:22:23 +0200

    Head & Footer
    Per el momento, está creat tot pel damunt. Quedaria insertar els logos, fer que el disseny sigui responsive i crear els enllaços en el footer. També quedaria organitzar la grid del footer per tal de organitzar els diferents elements que hi contè.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { Header } from "./components/Home/Header";
+import Contacte from "./components/others/Contacte";
+import Error404 from "./components/others/Error404";
+import FAQ from "./components/others/FAQ";
+import './App.css';
+import Footer from "./components/Home/Footer";
+import Sponsors from "./components/Home/Sponsors";
+
 
 export default function App() {
-    return <Header items={[{ name: 'Home', url: 'test' }]}/>;
+    return(
+        <div className="App">
+             <Router>
+             
+             <Header inside={[
+            { name: 'Home', url: '/'},
+            { name: 'Dates', url: 'Timetable' },
+            { name: 'Sponsors', url: 'Sponsors' }
+        
+    
+            ]} outsides={[
+                { name: 'FAQ', url: '/FAQ' },
+                { name: 'Contacte', url: '/Contacte' }
+            ]} />
+            <Routes>
+                <Route exact path="/" element = {<FAQ />}/>
+                <Route exact path="/FAQ" element = {<FAQ />}/>
+                <Route exact path="/Contacte" element = {<Contacte />}/>
+                <Route path="*" element = {<Error404 />}/>
+          </Routes>
+          </Router>
+
+            <Sponsors />
+            <Footer />
+
+        </div>
+    );
 }
---
+++ src/components/Home/Sponsors.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sponsors = () => {
+  return (
+    <div id='test'>
+      <h1>Esto es un TEST. El component del test es Sponsors</h1>
+    </div>
+  )
+}
+
+export default Sponsors
