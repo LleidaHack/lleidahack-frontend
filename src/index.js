35454de28a7d83f04d158f5679001f125b87f1d0
marcgj <mgaspjoval2002@gmail.com>
2023-05-11 20:46:55 +0200

    bootstrap icons added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
