1dde2fc7f87a6084e3dba29810fe7e61e46df7b5
marcgj <mgaspjoval2002@gmail.com>
2023-06-28 16:24:35 +0200

    Filled from a list

--- src/Pages/Profile/Medals.js
+++ src/Pages/Profile/Medals.js
@@ -4,26 +4,17 @@ import medal from "../../icons/medal_example.png";
 
 const Medals = () => {
     
+    const medals = [medal, medal, medal, medal, medal, medal]
 
     return ( <div className="container w-100 p-bg-red p-4 pb-5">
               <h2>Medalles</h2>
               <div className="container mt-2 text-center m-0">
                 <div className="row m-auto">
-                  <div className="col-4 mt-2 text-center ">
-                    <img className=" m-auto" src={medal} alt="" />
-                  </div>
-                  <div className="col-4 mt-2 text-center ">
-                    <img className=" m-auto" src={medal} alt="" />
-                  </div>
-                  <div className="col-4 mt-2 text-center ">
-                    <img className=" m-auto" src={medal} alt="" />
-                  </div>
-                  <div className="col-4 mt-2 text-center ">
-                    <img className=" m-auto" src={medal} alt="" />
-                  </div>
-                  <div className="col-4 mt-2 text-center ">
-                    <img className=" m-auto" src={medal} alt="" />
-                  </div>
+                  {medals.map((element, i) => (
+                    <div className="col-4 mt-2 text-center ">
+                      <img className=" m-auto" src={element} alt="" />
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>)
