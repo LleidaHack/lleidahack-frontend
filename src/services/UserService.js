502868385ff0ec8bcdfa6fd6319162d7b82cfc7d
elver <joelros2003@gmail.com>
2023-07-22 11:52:08 +0200

    fix user(s) i possible canvi de format
    esperant confirmació per seguir

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,27 @@
-url="https://backend.lleidahack.dev/"
+//url="https://backend.lleidahack.dev/"
+
+export async function signupUser(user) {
+    return fetch('https://backend.lleidahack.dev/user/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('response  ', data);
+        return data;
+    })
+    .catch(error => {
+        console.warn(error);
+        return[]
+    });
+}
+
 export async function getAllUsers() {
     try{
-        const response = await fetch(url+'users');
+        const response = await fetch('https://backend.lleidahack.dev/user/all');
         // console.log('response  ', response)
         return await response.json();
     }catch(error) {
@@ -11,23 +31,7 @@ export async function getAllUsers() {
 
 export async function getUserById(id) {
     try{
-        const response = await fetch(`https://backend.lleidahack.dev/users/${id}`);
-        // console.log('response  ', response)
-        return await response.json();
-    }catch(error) {
-        return [];
-    }   
-}
-
-export async function addUser(user) {
-    try{
-        const response = await fetch('https://backend.lleidahack.dev/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        });
+        const response = await fetch(`https://backend.lleidahack.dev/user/${id}`);
         // console.log('response  ', response)
         return await response.json();
     }catch(error) {
@@ -37,7 +41,7 @@ export async function addUser(user) {
 
 export async function updateUser(user) {
     try{
-        const response = await fetch(`https://backend.lleidahack.dev/users/${user.id}`, {
+        const response = await fetch(`https://backend.lleidahack.dev/user/${user.id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -53,11 +57,27 @@ export async function updateUser(user) {
 
 export async function deleteUser(id) {
     try{
-        const response = await fetch(`https://backend.lleidahack.dev/users/${id}`, {
+        const response = await fetch(`https://backend.lleidahack.dev/user/${id}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
             }
+        });
+        // console.log('response  ', response)
+        return await response.json();
+    }catch(error) {
+        return [];
+    }   
+}
+
+export async function addUser(user) {
+    try{
+        const response = await fetch('https://backend.lleidahack.dev/user/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
         });
         // console.log('response  ', response)
         return await response.json();
