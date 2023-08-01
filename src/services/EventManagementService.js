d5fa507e270a6406ec45f78676bb6ca824bc49a3
elver <joelros2003@gmail.com>
2023-08-01 20:22:18 +0200

    event management

---
+++ src/services/EventManagementService.js
@@ -0,0 +1,66 @@
+export async function registerHackerToEvent(hacker_id,event_id) {
+    return fetch(`https://backend.lleidahack.dev/eventmanagment/${event_id}/register/${hacker_id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('response: ', data);
+        return data;
+    })
+    .catch(error => {
+        console.warn(error);
+        return [];
+    });
+}
+
+export async function unregisterHackerToEvent(hacker_id,event_id) {
+    return fetch(`https://backend.lleidahack.dev/eventmanagment/${event_id}/unregister/${hacker_id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('response: ', data);
+        return data;
+    })
+    .catch(error => {
+        console.warn(error);
+        return [];
+    });
+}
+
+export async function getEventStatus(event_id) {
+    return fetch(`https://backend.lleidahack.dev/eventmanagment/${event_id}/status`)
+    .then(response => response.json())
+    .then(data => {
+        console.log('response: ', data);
+        return data;
+    })
+    .catch(error => {
+        console.warn(error);
+        return [];
+    });
+}
+
+export async function hackerEatsFoodFrom(hacker_id,meal_id,event_id) {
+    return fetch(`https://backend.lleidahack.dev/eventmanagment/${event_id}/eat/${meal_id}/${hacker_id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('response: ', data);
+        return data;
+    })
+    .catch(error => {
+        console.warn(error);
+        return [];
+    });
+}
