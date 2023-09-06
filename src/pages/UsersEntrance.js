1c9a7fcd2e7e5e83469a2ecb9a26c9b6a7d73249
Big-Lolo <Big-Lolo@users.noreply.github.com>
2023-09-06 18:13:49 +0000

    Apply formatting changes

--- src/pages/UsersEntrance.js
+++ src/pages/UsersEntrance.js
@@ -3,16 +3,14 @@ import Entrances from "../components/UserEnters/UserEnter.js";
 import Header from "../components/Header/Header.js";
 import Footer from "../components/Footer/Footer.js";
 
-
-
 const FAQPage = () => {
-    return (
-      <div>
-        <Header />
-        <Entrances/>
-        <Footer />
-      </div>
-    );
-  };
-  
-  export default FAQPage;
+  return (
+    <div>
+      <Header />
+      <Entrances />
+      <Footer />
+    </div>
+  );
+};
+
+export default FAQPage;
