3edb43cb37a382b76ba7094f3b53128a39146453
Quiracle <57363628+Quiracle@users.noreply.github.com>
2023-09-11 13:24:55 +0200

    rutes globals

--- src/pages/UsersEntrance.js
+++ src/pages/UsersEntrance.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Entrances from "../components/UserEnters/UserEnter.js";
-import Header from "../components/Header/Header.js";
-import Footer from "../components/Footer/Footer.js";
+import Entrances from "src/components/UserEnters/UserEnter.js";
+import Header from "src/components/Header/Header.js";
+import Footer from "src/components/Footer/Footer.js";
 
 const FAQPage = () => {
   return (
