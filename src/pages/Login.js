27a9e53922dcb09076408b1b06a93039e9d18ac7
Quiracle <57363628+Quiracle@users.noreply.github.com>
2023-09-11 12:12:01 +0200

    pito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import LoginPage from "../components/Login/Login";
+import Header from "src/components/Header/Header";
+import Footer from "src/components/Footer/Footer";
+import LoginPage from "src/components/Login/Login";
 
 const Login = () => {
   return (
