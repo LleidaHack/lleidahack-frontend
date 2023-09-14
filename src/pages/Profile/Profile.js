85de37cf049211800bada2df1287b69f0bba1e41
marcgj <mgaspjoval2002@gmail.com>
2023-09-14 13:50:48 +0200

    some fixes

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -22,11 +22,6 @@ import { getHackerById } from "src/services/HackerService";
 const Profile = () => {
   const { hacker_id } = useParams();
 
-  const name = "Nom cognom";
-  const usrImage = userIcon;
-
-  const yearsMember = "x";
-
   const startDate = new Date(2022, 10, 25);
   const endDate = new Date(2022, 10, 27);
 
@@ -74,14 +69,14 @@ const Profile = () => {
     <>
       <div className="p-bg-black text-white">
         <Header />
-        <div className="container-fluid container-xxl">
+        <div className="container-fluid container-xxl m-auto">
           {/* User info and qr */}
           <div className="row align-middle mx-auto my-3">
             {/* User Image */}
             <div className="col-12 col-xl-4 m-auto text-center">
               {hacker ? (
                 <img
-                  style={{ height: `150px` }}
+                  style={{ aspectRatio: "1/1", width: "15vh" }}
                   className="bg-white border rounded-circle m-auto"
                   src={"https://xsgames.co/randomusers/avatar.php?g=pixel"}
                 />
@@ -90,42 +85,51 @@ const Profile = () => {
               )}
             </div>
             {/* Center Column */}
-            <div className="col-12 col-xl-4 px-0 my-3 text-center">
-              <div className="row ">
-                <h3 className="text-center">Benvingut/da, hacker!</h3>
-              </div>
-              <div className="row my-3">
-                <h1>
-                  - {hacker ? hacker.name : <HSkeleton width={"50%"} inline />}{" "}
-                  -
-                </h1>
-              </div>
-              <div className="row">
-                <span className="text-center">
-                  Membre desde fa{" "}
-                  {hacker ? "PENDENT" : <HSkeleton width={"5%"} inline />} anys
-                </span>
+            <div className="col-12 col-xl-4 px-0 my-3 text-center d-flex align-items-center justify-content-center">
+              <div className="">
+                <div className="row">
+                  <h3 className="text-center">Benvingut/da, hacker!</h3>
+                </div>
+                <div className="row my-3">
+                  <h1>
+                    -{" "}
+                    {hacker ? hacker.name : <HSkeleton width={"50%"} inline />}{" "}
+                    -
+                  </h1>
+                </div>
+                <div className="row">
+                  <span className="text-center">
+                    Membre desde fa{" "}
+                    {hacker ? "PENDENT" : <HSkeleton width={"5%"} inline />}{" "}
+                    anys
+                  </span>
+                </div>
               </div>
             </div>
             {/* QR Column */}
             <div className="col-12 col-xl-4 mx-auto">
               {hacker ? (
-                <a href="#" onClick={handleShowQR}>
-                  <div className="container qr-container p-bg-primary p-2 text-center m-auto">
-                    <div className="row">
-                      <div className="col-6 my-auto col-sm-12">
-                        Mostra el teu tiquet
-                      </div>
-                      <div className="col-6 col-sm-12">
-                        <img
-                          width="66px"
-                          className="m-auto px-2"
-                          src={qrIcon}
-                        />
+                <div className="col-12 col-xl-4 mx-auto my-auto">
+                  <a
+                    className="text-decoration-none text-light"
+                    onClick={handleShowQR}
+                  >
+                    <div className="container qr-container p-bg-primary p-2 text-center m-auto">
+                      <div className="row">
+                        <div className="col-6 my-auto col-sm-12">
+                          Mostra el teu tiquet
+                        </div>
+                        <div className="col-6 col-sm-12 my-auto">
+                          <img
+                            style={{ aspectRatio: "1/1", width: "70%" }}
+                            className="px-2 mx-auto"
+                            src={qrIcon}
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </a>
+                  </a>
+                </div>
               ) : (
                 <HSkeleton height={"100%"} />
               )}
@@ -141,7 +145,7 @@ const Profile = () => {
           {team ? <Team team={team} /> : <></>}
 
           {/* Calendar and Achievements */}
-          <div className="row m-5 gy-5 bottom-container text-center m-auto">
+          <div className="row m-5 gy-5 bottom-container text-center mx-auto">
             <div className="col-12 col-xl-6">
               <Medals />
             </div>
