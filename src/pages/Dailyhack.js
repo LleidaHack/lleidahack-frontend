458ec881ba55422c430b35c8eca9527c443c70e6
Big-Lolo <Big-Lolo@users.noreply.github.com>
2023-09-16 01:09:58 +0000

    Apply formatting changes

--- src/pages/Dailyhack.js
+++ src/pages/Dailyhack.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Header from "src/components/Header/Header";
 import Footer from "src/components/Footer/Footer";
-import Dailyhacks from "src/components/Dailyhack/DailyHacks"
-
+import Dailyhacks from "src/components/Dailyhack/DailyHacks";
 
 const Dailyhack = () => {
-    return (
-      <div>
-        <Header />
-        <Dailyhacks />
-        <Footer />
-      </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Dailyhacks />
+      <Footer />
+    </div>
+  );
 };
 
 export default Dailyhack;
