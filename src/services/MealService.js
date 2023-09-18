45e2b1c67a06c31b963836b386c84efd5e86ab13
elver <joelros2003@gmail.com>
2023-09-19 00:16:28 +0200

    meal update

--- src/services/MealService.js
+++ src/services/MealService.js
@@ -1,96 +1,41 @@
+import { fetchPlus } from "src/modules/fetchModule";
+
 export async function getMeals(id) {
-  console.log(localStorage.getItem("userToken"));
-  return fetch(process.env.REACT_APP_DOMAIN + `/meal/${id}/all`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/meal/${id}/all`,
+    hasUserauth: true
+  });
 }
 
 export async function getMealById(id, meal_id) {
-  console.log(localStorage.getItem("userToken"));
-  return fetch(process.env.REACT_APP_DOMAIN + `/meal/${id}/${meal_id}`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/meal/${id}/${meal_id}`,
+    hasUserauth: true
+  });
 }
 
 export async function updateMeal(id, meal) {
-  return fetch(process.env.REACT_APP_DOMAIN + `/meal/${id}/${meal.id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-    body: JSON.stringify(meal),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/meal/${id}/${meal.id}`,
+    Method: "PUT",
+    hasUserauth: true,
+    Body: meal
+  });
 }
 
 export async function deleteMeal(id, meal_id) {
-  return fetch(process.env.REACT_APP_DOMAIN + `/meal/${id}/${meal_id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/meal/${id}/${meal_id}`,
+    Method: "DELETE",
+    hasUserauth: true,
+  });
 }
 
 export async function createMeal(meal) {
-  return fetch(process.env.REACT_APP_DOMAIN + `/meal/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-    body: JSON.stringify(meal),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: "/meal/",
+    Method: "POST",
+    hasUserauth: true,
+    Body: meal
+  });
 }
