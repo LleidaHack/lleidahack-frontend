e06f9c0f5aca9507c65dadbb990ae2fbd8fee396
elver <joelros2003@gmail.com>
2023-09-18 12:26:05 +0200

    lleidahacker rework

--- src/services/LleidaHackerGroupService.js
+++ src/services/LleidaHackerGroupService.js
@@ -1,182 +1,72 @@
+import { fetchPlus } from "src/modules/fetchModule";
+
 export async function getAllLleidaHackerGroups() {
-  return fetch(process.env.REACT_APP_DOMAIN + "/lleidahacker/group/all", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: "/lleidahacker/group/all",
+    hasUserauth: true
+  });
 }
 
 export async function getLleidaHackerGroupById(lleidaHacker_group_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN +
-      `/lleidahacker/group/${lleidaHacker_group_id}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/group/${lleidaHacker_group_id}`,
+    hasUserauth: true
+  });
 }
 
 export async function deleteLleidaHackerGroup(lleidaHacker_group_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN +
-      `/lleidahacker/group/${lleidaHacker_group_id}`,
-    {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/group/${lleidaHacker_group_id}`,
+    Method: "DELETE",
+    hasUserauth: true
+  });;
 }
 
 export async function addLleidaHackerGroup(lleidaHacker_group) {
-  return fetch(process.env.REACT_APP_DOMAIN + "/lleidahacker/group/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-    body: JSON.stringify(lleidaHacker_group),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: "/lleidahacker/group/",
+    Method: "POST",
+    hasUserauth: true,
+    Body: lleidaHacker_group
+  });
 }
 
 export async function getLleidaHackerGroupMembers(lleidaHacker_group_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN +
-      `/lleidahacker/group/${lleidaHacker_group_id}/members`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/group/${lleidaHacker_group_id}/members`,
+    hasUserauth: true
+  });
 }
 
 export async function addLleidaHackerToGroup(
   lleidaHacker_id,
   lleidaHacker_group_id,
 ) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN +
-      `/lleidahacker/group/${lleidaHacker_group_id}/members/${lleidaHacker_id}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/group/${lleidaHacker_group_id}/members/${lleidaHacker_id}`,
+    Method: "POST",
+    hasUserauth: true
+  });
 }
 
 export async function removeLleidaHackerFromGroup(
   lleidaHacker_id,
   lleidaHacker_group_id,
 ) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN +
-      `/lleidahacker/group/${lleidaHacker_group_id}/members/${lleidaHacker_id}`,
-    {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/group/${lleidaHacker_group_id}/members/${lleidaHacker_id}`,
+    Method: "DELETE",
+    hasUserauth: true
+  });
 }
 
 export async function setLleidaHackerGroupLeader(
   lleidaHacker_id,
   lleidaHacker_group_id,
 ) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN +
-      `/lleidahacker/group/${lleidaHacker_group_id}/leader/${lleidaHacker_id}`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/group/${lleidaHacker_group_id}/leader/${lleidaHacker_id}`,
+    Method: "PUT",
+    hasUserauth: true
+  });
 }
--- src/services/LleidaHackerService.js
+++ src/services/LleidaHackerService.js
@@ -1,211 +1,73 @@
+import { fetchPlus } from "src/modules/fetchModule";
+
 export async function signupLleidaHacker(lleidaHacker) {
-  return fetch(process.env.REACT_APP_DOMAIN + "/lleidahacker/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(lleidaHacker),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: "/lleidahacker/signup",
+    Method: "POST",
+    Body: lleidaHacker
+  });
 }
 
 export async function getAllLleidaHackers() {
-  return fetch(process.env.REACT_APP_DOMAIN + "/lleidahacker/all", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: "/lleidahacker/all",
+    hasUserauth: true
+  });
 }
 
 export async function getLleidaHackerById(lleidaHacker_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN + `/lleidahacker/${lleidaHacker_id}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/${lleidaHacker_id}`,
+    hasUserauth: true
+  });
 }
 
 export async function updateLleidaHacker(lleidaHacker) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN + `/lleidahacker/${lleidaHacker.id}`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-      body: JSON.stringify(lleidaHacker),
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/${lleidaHacker.id}`,
+    Method: "PUT",
+    hasUserauth: true,
+    Body: lleidaHacker
+  });
 }
 
 export async function deleteLleidaHacker(lleidaHacker_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN + `/lleidahacker/${lleidaHacker_id}`,
-    {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/${lleidaHacker_id}`,
+    Method: "DELETE",
+    hasUserauth: true
+  });
 }
 
-/*export async function addLleidaHacker(lleidaHacker) {
-  return fetch(process.env.REACT_APP_DOMAIN + "/lleidahacker/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("userToken"),
-    },
-    body: JSON.stringify(lleidaHacker),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
-}*/
 
 export async function acceptLleidaHacker(lleidaHacker_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN + `/lleidahacker/${lleidaHacker_id}/accept`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/${lleidaHacker_id}/accept`,
+    Method: "POST",
+    hasUserauth: true
+  });
 }
 
 export async function rejectLleidaHacker(lleidaHacker_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN + `/lleidahacker/${lleidaHacker_id}/reject`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/${lleidaHacker_id}/reject`,
+    Method: "POST",
+    hasUserauth: true
+  });
 }
 
 export async function activateLleidaHacker(lleidaHacker_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN + `/lleidahacker/${lleidaHacker_id}/activate`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/${lleidaHacker_id}/activate`,
+    Method: "POST",
+    hasUserauth: true
+  });
 }
 
 export async function deactivateLleidaHacker(lleidaHacker_id) {
-  return fetch(
-    process.env.REACT_APP_DOMAIN +
-      `/lleidahacker/${lleidaHacker_id}/deactivate`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("userToken"),
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("response: ", data);
-      return data;
-    })
-    .catch((error) => {
-      console.warn(error);
-      return [];
-    });
+  return fetchPlus({
+    Url: `/lleidahacker/${lleidaHacker_id}/deactivate`,
+    Method: "POST",
+    hasUserauth: true
+  });
 }
