7588a0d8db9d1c7de248290e39aa9b2748db054a
Quiracle <57363628+Quiracle@users.noreply.github.com>
2023-09-21 22:54:03 +0200

    use old inscripcio

--- src/components/Inscripcio/Inscripcio.js
+++ src/components/Inscripcio/Inscripcio.js
@@ -1,205 +1,256 @@
-import Header from "src/components/Header/Header.js";
-import Footer from "src/components/Footer/Footer.js";
-import "src/components/Forms/HackerForm.css";
-import "formik-stepper/dist/style.css";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { useState } from "react";
+import React, { useState } from "react";
+import "src/components/Inscripcio/Inscripcio.css";
 
-import * as Yup from "yup";
-import { CheckBoxField, FormikStepper, InputField, SelectField } from "formik-stepper";
-import { signupHacker } from "src/services/HackerService";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-
+import * as Yup from "yup";
+import { SelectField } from "formik-stepper";
+import { registerHackerToEvent } from "src/services/EventManagementService";
+import { getHackeps } from "src/services/EventService";
 
 const validationSchema = Yup.object().shape({
-  study: Yup.string().required("Nom requerit"),
-  school: Yup.string().required("Cognoms requerits"),
-  from: Yup.string().required("Cognoms requerits"),
-
-  email: Yup.string()
-    .email("The email must be a valid email address.")
-    .required("The Email field is required"),
-  phone: Yup.string().required("Telèfon requerit"),
-  shirtSize: Yup.string().required("Talla de camiseta requerida"),
-  nickname: Yup.string().required("Nickname requerit"),
+  studies: Yup.string().required("Aquest camp és obligatori"),
+  center: Yup.string().required("Aquest camp és obligatori"),
+  location: Yup.string().required("Aquest camp és obligatori"),
+  size: Yup.string().required("Aquest camp és obligatori"),
+  food: Yup.string().required("Aquest camp és obligatori"),
+  meet: Yup.string().required("Aquest camp és obligatori"),
 });
 
+const InscripcioForm = () => {
+  const sizeOptions = [
+    { value: "S", label: "S" },
+    { value: "M", label: "M" },
+    { value: "L", label: "L" },
+    { value: "XL", label: "XL" },
+    { value: "XXL", label: "XXL" },
+  ];
 
+  const meetOptions = [
+    { value: "Xarxes socials", label: "Xarxes socials" },
+    { value: "Un amic", label: "Un amic" },
+    { value: "Altres edicions", label: "Altres edicions" },
+    { value: "Cartells publicitaris", label: "Cartells publicitaris" },
+    { value: "Altre mitjà", label: "Altre mitjà" },
+  ];
 
-export const InscripcioForm = () => {
-  const [avatar, setAvatar] = useState(null);
-  const [urlImage, setUrlImage] = useState("");
-  const [isUrl, setIsUrl] = useState(false);
-  const onSubmit = async (values, { setSubmitting }) => {
-    const pfp = isUrl ? urlImage : avatar;
-    const hacker = {
-      study: [values.firstName, values.lastName].join(" "),
-      school: values.nickname,
-      from: "string",
-      food_restrictions: "",
-      email: values.email,
-      telephone: values.phone,
-      address: "",
-      shirt_size: values.shirtSize,
-      meet_us: values.meet,
-      image: pfp,
-      is_image_url: isUrl,
-      github: "",
-      linkedin: "",
+  const handleSubmit = (values) => {
+    //TODO: estudis, centre, lloc, coneixer? on va això?
+    console.log(values);
+    const data = {
+      shirt_size: values.size,
+      food_restrictions: values.food,
+      cv: cvFile,
+      description: values.cvinfo,
+      github: values.github,
+      linkedin: values.linkedin,
+      update_user: true,
     };
-    console.log(hacker);
-    signupHacker(hacker);
-    setSubmitting(false);
+    registerHackerToEvent(localStorage.getItem("userID"), getHackeps(), data);
+    //TODO: posar feedback
   };
 
-  const handleImageChange = (event) => {
-    setAvatar(event.base64);
-    setIsUrl(false);
+  const [cvFile, setCvFile] = useState("");
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setCvFile(file);
+    console.log(file);
   };
-  const handleImageUrlChange = (event) => {
-    setUrlImage(event.target.value);
-    setIsUrl(true);
+
+  const clearFile = () => {
+    setCvFile("");
+    // Clear the input field to allow selecting the same file again
+    const inputElement = document.getElementById("cvinfo_file");
+    if (inputElement) {
+      inputElement.value = "";
+    }
   };
 
   return (
-    <div id="hackerForm" className="custom-form">
-      <FormikStepper
-        /// Accept all Formik props
-        onSubmit={onSubmit}
-        isSubmiting={true}
-        initialValues={{
-          study: "",
-          school: "",
-          from: "",
-          email: "",
-          nickname: "",
-        }}
-        validationSchema={validationSchema}
-        withStepperLine /// false as default and If it is false, it hides stepper line
-        nextButton={{
-          label: "Següent",
-          style: { background: "var(--primary)" },
-        }}
-        prevButton={{
-          label: "Enrere",
-          style: { background: "var(--primary)" },
-        }}
-        submitButton={{
-          label: "Envia",
-          style: { background: "var(--primary)" },
-        }}
-      >
-        <FormikStepper.Step label="Formació">
-          <Row>
-        
-            <Col>
-              <h1 className="white-color">Formació</h1>
-              <InputField name="study" type="text" label="Que estudies o has estudiat?" />
-              <InputField name="school" type="text" label="Centre d'estudis" />
-              <InputField name="from" type="text" label="D'on vens?" />
-             
-            </Col>
-          </Row>
-        </FormikStepper.Step>
-        <FormikStepper.Step label="Detalls">
-          <Row>
-           
-            <Col>
-              <h1 className="white-color">Detalls</h1>
-             
-              <div>
-                <SelectField
-                  name="shirtSize"
-                  label="Talla de samarreta"
-                  options={[
-                    { value: "S", label: "S" },
-                    { value: "M", label: "M" },
-                    { value: "L", label: "L" },
-                    { value: "XL", label: "XL" },
-                  ]}
-                />
-              </div>
-              <div>
-                <SelectField
-                  name="meet"
-                  label="Com ens has conegut?"
-                  options={[
-                    { value: "Xarxes Socials", label: "XXSS" },
-                    { value: "Amic", label: "friends" },
-                    { value: "Altres edicions", label: "last_editions" },
-                    { value: "Altre mitjà", label: "other" },
-                  ]}
-                />
-              </div>
-              <InputField name="alergies" type="text" label="Tens alguna restricció alimentària o alèrgia?" />
-            </Col>
-          </Row>
-        </FormikStepper.Step>
-        <FormikStepper.Step label="Perfil públic">
-        <Row>
-           
-           <Col>
-             <h1 className="white-color">Perfil públic</h1>
-             <p className="white-color"> Vols que les empreses de Lleida et coneguin? (Opcional)</p>
-            <br></br>
-             
-             <InputField name="linkedin" type="text" label="Enllaç de LinkedIn" />
-             <InputField name="github" type="text" label="Enllaç de Github" />
-             <InputField name="devpost" type="text" label="Enllaç de Devpost" />
-
-             <label htmlFor="textareaField" className="white-color">Tens expeciència en altres hackatons?</label>
-                <Field
-                    as="textarea" // Usa "textarea" para el campo de texto
-                    id="textareaField"
-                    name="Comment"
-                    rows={4} // Personaliza el número de filas según tus necesidades
-                    placeholder="Tens expeciència en altres hackatons? Algun projecte personal que vulguis compartir? Explica'ns què t'apassiona i deixa aquí els enllaços de les teves xarxes socials."
-                    className="BoxForm"
-                />
-           </Col>
-         </Row>
-        </FormikStepper.Step>
-        <FormikStepper.Step label="Definir Grups">
-        <Row>
-           
-           <Col>
-             <h1 className="white-color">Definir Grups</h1>
-            <br></br>
-             
-             
-           </Col>
-         </Row>
-        </FormikStepper.Step>
-        <FormikStepper.Step label="Termes i condicions">
-        <Row>
-           
-           <Col>
-           <h1 className="white-color">Termes i Condicions</h1>
-           <br></br>
-            <div className="TermsSpace">
-                <p className="white-color box"> Al marcar el seguent check, acceptes els termes i condicións de l'esdeveniment juntament amb el compromís d'asistir a l'esdeveniment.</p>
-                <p className="white-color box">Podrás trobar els terminis i condicions en els seguents apartats</p>
-                <p className="white-color box">Terminis i condicions</p>
-                <br></br>
-                
+    <div className="container-all">
+      <br />
+      <h1 className="title-contacte">Inscripció HackEPS 2023</h1>
+      <div className="form-container">
+        <Formik
+          initialValues={{
+            studies: "",
+            center: "",
+            location: "",
+            size: "",
+            food: "",
+            cvinfo: "",
+            meet: "",
+            linkedin: "",
+            github: "",
+            devpost: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <div className="formik-field">
+              <label htmlFor="studies">Què estudies o has estudiat?</label>
+              <Field type="text" id="studies" name="studies" />
+              <ErrorMessage
+                name="studies"
+                component="div"
+                className="error-message"
+              />
             </div>
-            <label>
-          <input
-            name="healthBackground"
-            type="checkbox"
-            value="test2"
-          />
-          Acceptes els termes i condicion per a poder participar a la 7ª edicio de la Hackeps
-        </label>
-            
-           </Col>
-         </Row>
-        </FormikStepper.Step>
-      </FormikStepper>
+
+            <div className="formik-field">
+              <label htmlFor="center">Centre d'estudis:</label>
+              <Field type="text" id="center" name="center" />
+              <ErrorMessage
+                name="center"
+                component="div"
+                className="error-message"
+              />
+            </div>
+
+            <div className="formik-field">
+              <label htmlFor="location">D'on vens?:</label>
+              <Field type="text" id="location" name="location" />
+              <ErrorMessage
+                name="location"
+                component="div"
+                className="error-message"
+              />
+            </div>
+
+            <div className="formik-field">
+              <SelectField
+                id="size"
+                name="size"
+                label="Talla de samarreta:"
+                options={sizeOptions}
+                placeholder="La meva talla de samarreta és..."
+              />
+              <ErrorMessage
+                name="size"
+                component="div"
+                className="error-message"
+              />
+            </div>
+
+            <div className="formik-field">
+              <label htmlFor="food">
+                Tens alguna restricció alimentària o alèrgia?
+              </label>
+              <Field type="text" id="food" name="food" />
+              <ErrorMessage
+                name="food"
+                component="div"
+                className="error-message"
+              />
+            </div>
+
+            <div className="formik-field">
+              <SelectField
+                id="meet"
+                name="meet"
+                options={meetOptions}
+                label="Com ens has conegut?"
+                placeholder="Us he conegut per..."
+              />
+            </div>
+            <div className="formik-field">
+              <label htmlFor="cvinfo_links">
+                Vols que les empreses de Lleida et coneguin? (Opcional)
+              </label>
+              <p className="subtitle">
+                Tens expeciència en altres hackatons? Algun projecte personal
+                que vulguis compartir? Explica'ns què t'apassiona i deixa aquí
+                els enllaços de les teves xarxes socials.
+              </p>
+              <Field
+                as="textarea"
+                id="cvinfo_links"
+                name="cvinfo_links"
+                rows="4"
+              />
+
+              {/* Agrupamos los campos de LinkedIn, GitHub y Devpost uno debajo del otro */}
+              <div className="subfields-container">
+                <div className="subfield">
+                  <Field
+                    type="text"
+                    id="linkedin"
+                    name="linkedin"
+                    placeholder="Enllaç de LinkedIn"
+                  />
+                  <ErrorMessage
+                    name="linkedin"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+              </div>
+
+              <div className="subfields-container">
+                <div className="subfield">
+                  <Field
+                    type="text"
+                    id="github"
+                    name="github"
+                    placeholder="Enllaç de GitHub"
+                  />
+                  <ErrorMessage
+                    name="github"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+              </div>
+
+              <div className="subfields-container">
+                <div className="subfield">
+                  <Field
+                    type="text"
+                    id="devpost"
+                    name="devpost"
+                    placeholder="Enllaç de Devpost"
+                  />
+                  <ErrorMessage
+                    name="devpost"
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="file-input-container">
+              <label htmlFor="cvinfo_file">Adjunta el teu CV (Opcional)</label>
+              <input
+                type="file"
+                id="cvinfo_file"
+                name="cvinfo_file"
+                onChange={handleFileChange}
+              />
+              {cvFile && (
+                <div className="file-info">
+                  <span className="file-name">{cvFile.name}</span>
+                  <button
+                    type="button"
+                    className="delete-button"
+                    onClick={clearFile}
+                  >
+                    &#10005;
+                  </button>
+                </div>
+              )}
+            </div>
+            <div className="button-submit-container">
+              <button className="button-submit" type="submit">
+                Enviar
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
 
 export default InscripcioForm;
-
