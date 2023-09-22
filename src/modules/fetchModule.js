20f26cfca783376ce4e97fcff208af00a7d7043a
elver <joelros2003@gmail.com>
2023-09-22 12:32:49 +0200

    pass update

--- src/modules/fetchModule.js
+++ src/modules/fetchModule.js
@@ -26,7 +26,8 @@ export async function fetchPlus({
     query = `?${Object.entries(Query)
       .map(([key, value]) => `${key}=${value}`)
       .join("&")}`;
-  if (process.env.REACT_APP_DEBUG === "true") console.log("headers: ", args);
+  if (process.env.REACT_APP_DEBUG === "true") 
+    console.log("headers: ", args);
   return fetch(process.env.REACT_APP_DOMAIN + Url + query, args)
     .then((response) => {
       if (process.env.REACT_APP_DEBUG === "true")
@@ -36,7 +37,8 @@ export async function fetchPlus({
       return response.json();
     })
     .then((data) => {
-      if (process.env.REACT_APP_DEBUG === "true") console.log("data: ", data);
+      if (process.env.REACT_APP_DEBUG === "true") 
+        console.log("data: ", data);
       if (saveLoginInfo) {
         localStorage.setItem("userToken", data.access_token);
         localStorage.setItem("userID", data.user_id);
