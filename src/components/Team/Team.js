343a92c9be261d5377ba54d55b6817982bbbd752
elver <joelros2003@gmail.com>
2023-09-22 11:52:29 +0200

    ratio

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -154,12 +154,13 @@ const Team = (props) => {
               <Col className="col-3" key={index}>
                 <div className="p-3 text-center bg-white">
                   <img
+                    style={{aspectRatio: "1/1"}}
                     className="team-member-image bg-black"
                     src={member.is_image_url ? member.image : "https://xsgames.co/randomusers/avatar.php?g=pixel"}
                     alt=""
                   />
                   <p className="team-member-name">{member.name}</p>
-                  <Button className="team-button" href={"/hackeps/perfil/"+member.id /* //TODO hardcoded*/ }> 
+                  <Button className="team-button" href={"/hackeps/perfil/"+member.id /* / /TODO hardcoded*/ }> 
                     Veure perfil
                   </Button>
                 </div>
