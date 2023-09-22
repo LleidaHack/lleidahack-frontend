86ad42757f2a2ddb948cf9ccad574169b1e27085
Quiracle <jsalattorres@gmail.com>
2023-09-22 11:00:14 +0200

    la tinc gordissima

--- src/modules/RequireAuth.js
+++ src/modules/RequireAuth.js
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import { checkToken } from "src/services/AuthenticationService";
 
 export default function RequireAuth({children}) {
     const [auth, setAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
     
-    checkToken().then((key) => {
-        setAuth(!key["success"]);
-      })
+    useEffect(() => {
+        (async () => {
+          await checkToken().then((key) => {
+            setAuth(key["success"]);
+
+          })
+        
+        setLoading(false)
+        console.log(auth)
+        })()
+    },[])
     
-    return auth? children : <Navigate to={"../login"}/>;
+    return loading ?  // The code that did the magic
+        <span>Loading...</span> : (auth?
+            children : <Navigate to={"/login"}/>) 
 }
