4105d053ee4be22077ef1f17c9d67020f2e8e1a6
Big_Lolo <95545807+Big-Lolo@users.noreply.github.com>
2023-09-24 22:53:44 +0200

    Confirm + Err
    Afegida la pantalla de confirmacio correcta indicant que s'ha enviat correctament el correu i un boto per a tornar a l'inici.
    
    Per altra banda, si hi hagués un error, s'indicaria per sota del correu
    
    Per a que detecti el succés, el missatge del endpoint ha de ser "Succes"

--- src/components/ForgetPassword/ForgetPassword.js
+++ src/components/ForgetPassword/ForgetPassword.js
@@ -16,24 +16,30 @@ const validationSchema = Yup.object().shape({
 
 
 const ForgetPassword = ({nextScreen}) => {
-    const [status, setStatus] = useState("none");
+    const [status, setStatus] = useState(false);
 
-    const handleSubmit = async (values) => {
+    const handleSubmit = async (values, { setSubmitting, setFieldError }) => {
         console.log("El email es:", values.email)
         const sendingQuest = await resetPassword(values.email)
-        
-        if(sendingQuest.message == "User not found"){
-            setStatus("notfound")
-        }else if(sendingQuest.message == "User not verified"){
-            setStatus("notverified")
-        }else if(sendingQuest.message == "Succes"){
-            setStatus("succes")
+        if(sendingQuest.message){
+            if(sendingQuest.message == "User not found"){
+                setFieldError("email", "No s'ha trobat un compte asociat a aquest correu. Comprova que estigui tot correcte.")
+            }else if(sendingQuest.message == "User not verified"){
+                setFieldError("email", "Sembla ser que encara no estas verificat. Comproba la teva bustia de spam.")
+            }else if(sendingQuest.message == "Succes"){
+                setStatus(true)
+            }
+        } else{
+            setFieldError("email", "Ha ocorregut un error, torna a intentar-ho més tard. Si l'error persisteix, contacta amb nosaltres")
+
         }
+        
       //Hay que  hacerlo de manera que las respuestas correctas o incorrectas generen una 2a pantalla de confirmacion, igual que con los dailyhacks
     };
   
     return (
       <div className="login-page">
+        {!status ? (
         <div className="content">
           <Container>
             <Row className="justify-content-center">
@@ -90,6 +96,51 @@ const ForgetPassword = ({nextScreen}) => {
             </Row>
           </Container>
         </div>
+            
+            ) : (
+            <div className="content">
+             <section className="informative">
+              <div className="Part2">
+                <div className="iconBox">
+                  <div className="wrapper">
+                    <svg
+                      className="checkmark"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 52 52"
+                    >
+                      {" "}
+                      <circle
+                        className="checkmark__circle"
+                        cx="26"
+                        cy="26"
+                        r="25"
+                        fill="none"
+                      />{" "}
+                      <path
+                        className="checkmark__check"
+                        fill="none"
+                        d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                      />
+                    </svg>
+                  </div>
+                  <h2>Enllaç enviat correctament</h2>
+                  <p>
+                    En breus rebràs un correu electrònic amb un enllaç per a recuperar el teu compte.
+                  </p>
+                  <p>
+                    <i>Si no ho reps, comproba la bustia de spam.</i>
+                  </p>
+                </div>
+
+                <div className="infbuttonok">
+                  <Link to="/">
+                    <button className="contacta">Tornar al Inici</button>
+                  </Link>
+                </div>
+              </div>
+            </section>
+            </div>
+            )}
       </div>
     );
   };
