158dfa9a05c720c6e1b2f92afae0a7be9b9035ec
marcgj <marcgj@users.noreply.github.com>
2023-09-25 17:13:27 +0000

    Apply formatting changes

--- src/pages/Verify.js
+++ src/pages/Verify.js
@@ -19,7 +19,7 @@ export default function Verify() {
       let mail;
       while (
         !(mail = window.prompt(
-          "Introdueix el teu mail per tornar a general el token"
+          "Introdueix el teu mail per tornar a general el token",
         ))
       );
 
