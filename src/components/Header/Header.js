66b64bb251243abda576b57e261ae080a95b9c4e
marcgj <mgaspjoval2002@gmail.com>
2023-09-25 18:43:21 +0200

    fixed default profile img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -122,8 +122,12 @@ const Header = () => {
               {localStorage.getItem("userToken") ? (
                 <li className="nav-item">
                   <Link to="" className="nav-link" onClick={togglePopup}>
-                    <div className="profileImage2">
-                      <img className="Profile2" src={icon}></img>
+                    <div className="profileImage2 d-flex">
+                      {icon !== "string" ? (
+                        <img className="Profile" src={icon}></img>
+                      ) : (
+                        <i class="fa-solid fa-user m-auto"></i>
+                      )}
                     </div>
                   </Link>
                 </li>
