6671253511749c4133fca53ccb1a00db2f1bc239
Arnaldopower <Arnaldopower@users.noreply.github.com>
2024-02-15 17:04:31 +0000

    Apply formatting changes

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -43,8 +43,11 @@ const Join = (props) => {
   }
 
   return (
-    <div style={{width: '50%'}}>
-      <div className="row container-fluid  p-bg-grey text-center m-auto lolospace" style={{ width: '80%', marginTop: '5%', marginBottom: '10%'}}>
+    <div style={{ width: "50%" }}>
+      <div
+        className="row container-fluid  p-bg-grey text-center m-auto lolospace"
+        style={{ width: "80%", marginTop: "5%", marginBottom: "10%" }}
+      >
         <div className="col-12 p-bg-grey">
           <div className="row">
             <img className="imgbox" src={logo} alt="" />
