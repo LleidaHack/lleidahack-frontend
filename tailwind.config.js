7c911bb156ee8c9ebcab0479b566b45c4dcc9374
bepes-code <75368713+bepes-code@users.noreply.github.com>
2024-05-01 07:59:57 +0200

    Add Colors COnfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FF7430',
+        'secondary': '#232323',
+        'CTA': '#F7F7F7',
+        'success': '#14D455',
+        'error': '#E55010',
+        'link': '#3366CC'
+      },
+    },
   },
   plugins: [],
 };
