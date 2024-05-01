ffef608e52671d7e16d1c40ef4f5f30a2f95c1e2
bepes-code <75368713+bepes-code@users.noreply.github.com>
2024-05-01 11:12:29 +0200

    Landing Updated

--- src/pages/Landing/HomeLanding.js
+++ src/pages/Landing/HomeLanding.js
@@ -1,10 +1,10 @@
-import navBar from "src/components/landing/Navbar/Navbar";
+import Navbar from "src/components/landing/Navbar/Navbar";
 import Footer from "src/components/landing/Footer/Footer";
 
 const HomeLanding = () => {
     return (
         <div>
-            <navBar />
+            <Navbar />
             <h1 className="text-secondary">Home</h1>
             <Footer />
         </div>
