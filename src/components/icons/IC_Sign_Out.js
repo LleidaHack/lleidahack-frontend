a511e312b750c19ab1316b78aa8a952acb12b925
elver5041 <elver5041@users.noreply.github.com>
2024-05-07 08:54:35 +0000

    Apply formatting changes

--- src/components/icons/IC_Sign_Out.js
+++ src/components/icons/IC_Sign_Out.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "src/components/icons/Icons.css"
+import "src/components/icons/Icons.css";
 import ICSignIn from "./IC_Sign_In";
 
 const IC_Sign_Out = () => {
-  return (
-    <ICSignIn rotateClass="in_to_out"/>
-  );
+  return <ICSignIn rotateClass="in_to_out" />;
 };
 
 export default IC_Sign_Out;
