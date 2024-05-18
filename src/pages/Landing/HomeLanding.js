73f7d53040770bbc1beea5d775ab708d0f8f3d19
bepes-code <75368713+bepes-code@users.noreply.github.com>
2024-05-18 19:53:06 +0200

    News Vertical display component imported

--- src/pages/Landing/HomeLanding.js
+++ src/pages/Landing/HomeLanding.js
@@ -1,11 +1,13 @@
 import Navbar from "src/components/landing/Navbar/Navbar";
 import Footer from "src/components/landing/Footer/Footer";
 import HeroSectionHeader from "src/components/landing/HeroSectionHeader/HeroSectionHeader";
+import NewsVerticalDisplay from "src/components/landing/NewsVerticalDisplay/NewsVerticalDisplay";
 const HomeLanding = () => {
   return (
     <div>
       <Navbar />
       <HeroSectionHeader />
+      <NewsVerticalDisplay />
       <Footer />
     </div>
   );
