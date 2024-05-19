b4fb5b4a36073ade3acf43482dad43b62e2be1b1
bepes-code <bepes-code@users.noreply.github.com>
2024-05-19 09:03:37 +0000

    Apply formatting changes

--- src/components/landing/NewsEventDisplay/NewsEventDisplay.jsx
+++ src/components/landing/NewsEventDisplay/NewsEventDisplay.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from "react";
 import logoLleidaHack from "../../../imgs/hackers_group.jpg";
-import { Link } from 'react-router-dom';
-const NewsEventDisplay = ({title, imgPath, name}) => {
-
+import { Link } from "react-router-dom";
+const NewsEventDisplay = ({ title, imgPath, name }) => {
   // const imagePath = require(`../../../assets/img/news/${imgPath}`).default;
   const imagePathTest = `../../../imgs/${imgPath}`;
 
   return (
-    <div className='w-full py-1 '>
-      <Link to= "news/`${name}`" className='no-underline text-secondaryLanding'>
-        <div className='flex p-3 bg-white shadow h-32'>
-            <img src={logoLleidaHack} alt={name} className='w-1/2 mx-1'/>
-            <p className='w-1/2 mx-1'>{title}</p>
+    <div className="w-full py-1 ">
+      <Link to="news/`${name}`" className="no-underline text-secondaryLanding">
+        <div className="flex p-3 bg-white shadow h-32">
+          <img src={logoLleidaHack} alt={name} className="w-1/2 mx-1" />
+          <p className="w-1/2 mx-1">{title}</p>
         </div>
       </Link>
-
     </div>
-  )
-}
+  );
+};
 
-export default NewsEventDisplay
+export default NewsEventDisplay;
--- src/components/landing/NewsVerticalDisplay/NewsVerticalDisplay.jsx
+++ src/components/landing/NewsVerticalDisplay/NewsVerticalDisplay.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import NewsEventDisplay from '../NewsEventDisplay/NewsEventDisplay'
+import React from "react";
+import NewsEventDisplay from "../NewsEventDisplay/NewsEventDisplay";
 
 const NewsVerticalDisplay = () => {
   return (
-    <div className='w-1/4 p-5'>
-      <div className=''>
-        <h2 className='bg-secondaryLanding text-CTALanding text-center p-1'>NOTICIES</h2>
-        <NewsEventDisplay title="Lleidahack celebra les seves primeres colonies" imgPath="hackers_group.jpg" name="test"/>
-        <NewsEventDisplay title="Lleidahack celebra les seves primeres colonies" imgPath="hackers_group.jpg" name="test"/>
-        <NewsEventDisplay title="Lleidahack celebra les seves primeres colonies" imgPath="hackers_group.jpg" name="test"/>
-        <a href='/news' className='text-primaryLanding no-underline'>Veure més...</a>
-        </div>
+    <div className="w-1/4 p-5">
+      <div className="">
+        <h2 className="bg-secondaryLanding text-CTALanding text-center p-1">
+          NOTICIES
+        </h2>
+        <NewsEventDisplay
+          title="Lleidahack celebra les seves primeres colonies"
+          imgPath="hackers_group.jpg"
+          name="test"
+        />
+        <NewsEventDisplay
+          title="Lleidahack celebra les seves primeres colonies"
+          imgPath="hackers_group.jpg"
+          name="test"
+        />
+        <NewsEventDisplay
+          title="Lleidahack celebra les seves primeres colonies"
+          imgPath="hackers_group.jpg"
+          name="test"
+        />
+        <a href="/news" className="text-primaryLanding no-underline">
+          Veure més...
+        </a>
+      </div>
     </div>
-  ) 
-}
+  );
+};
 
-export default NewsVerticalDisplay
+export default NewsVerticalDisplay;
