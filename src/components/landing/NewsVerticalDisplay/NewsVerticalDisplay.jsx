95927bfcb0e3b221635178b7d88b404a26c4e3fa
bepes-code <75368713+bepes-code@users.noreply.github.com>
2024-05-19 11:01:48 +0200

    updated NewsCompoennt

--- src/components/landing/NewsVerticalDisplay/NewsVerticalDisplay.jsx
+++ src/components/landing/NewsVerticalDisplay/NewsVerticalDisplay.jsx
@@ -6,10 +6,10 @@ const NewsVerticalDisplay = () => {
     <div className='w-1/4 p-5'>
       <div className=''>
         <h2 className='bg-secondaryLanding text-CTALanding text-center p-1'>NOTICIES</h2>
-        <NewsEventDisplay title="lorem ipsum" imgPath="hackers_group.jpg" name="test"/>
-        <NewsEventDisplay title="lorem ipsum" imgPath="hackers_group.jpg" name="test"/>
-        <NewsEventDisplay title="lorem ipsum" imgPath="hackers_group.jpg" name="test"/>
-        <a href='/news/{path}' className='text-primaryLanding no-underline'>Veure més...</a>
+        <NewsEventDisplay title="Lleidahack celebra les seves primeres colonies" imgPath="hackers_group.jpg" name="test"/>
+        <NewsEventDisplay title="Lleidahack celebra les seves primeres colonies" imgPath="hackers_group.jpg" name="test"/>
+        <NewsEventDisplay title="Lleidahack celebra les seves primeres colonies" imgPath="hackers_group.jpg" name="test"/>
+        <a href='/news' className='text-primaryLanding no-underline'>Veure més...</a>
         </div>
     </div>
   ) 
