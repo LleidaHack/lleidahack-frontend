f7c2f0e8d18947922ff4c0629aa1f6c84ed9fb15
elver <joelros2003@gmail.com>
2024-05-20 10:21:16 +0200

    fix: icons

--- src/pages/Landing/HomeLanding.js
+++ src/pages/Landing/HomeLanding.js
@@ -1,6 +1,9 @@
 import Navbar from "src/components/landing/Navbar/Navbar";
 import Footer from "src/components/landing/Footer/Footer";
 import HeroSectionHeader from "src/components/landing/HeroSectionHeader/HeroSectionHeader";
+import IC_LleidaHack from "src/components/icons/IC_LleidaHack";
+import IC_Sign_In from "src/components/icons/IC_Sign_In";
+import IC_Languages from "src/components/icons/IC_Languages";
 const HomeLanding = () => {
   return (
     <div>
