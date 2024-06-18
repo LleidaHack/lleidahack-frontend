7bf5d648fae64a7b00f6a7073c9cf1d4a497c254
Arnaldopower <116560232+Arnaldopower@users.noreply.github.com>
2024-06-18 22:34:06 +0200

    carbalho

--- src/components/landing/EventsSection/EventsSection.js
+++ src/components/landing/EventsSection/EventsSection.js
@@ -1,46 +1,47 @@
 import React from "react";
 import EventCard_five from "src/components/event_cards/eventCard_five/eventCard_five";
 import EventCard_four from "src/components/event_cards/eventCard_four/eventCard_four";
+import "src/components/landing/EventsSection/EventsSection.css";
 
 import background from "src/components/event_cards/defaultImage.jpg";
 import hack from "src/components/event_cards/Hack2023.jpg";
 
 const EventSection = () => {
   return (
-    <div className="max-w-[1080px] max-h-[544px]">
-      <div className="max-w-[1048px] max-h-[48px] bg-black mt-4 ml-4 ">
-        <p className="text-white font-space-mono font-bold tracking-[-0.02em] text-[32px] leading-[47.39px] ml-4">
-          EVENTS
+    <div className="mobile ampli max-w-[67.5rem] max-h-[34rem]">
+      <div className="max-w-[65.5rem] max-h-[3rem] bg-black mt-4 ml-4 ">
+        <p className="text-white font-space-mono font-bold tracking-[-0.02em] text-[2rem] leading-[2.962rem] ml-4">
+          ESDEVENIMENTS
         </p>
       </div>
 
-      <div className="flex ml-4">
+      <div className="flex ml-4 col">
         <EventCard_five
           title="HackEPS 2023"
           description="Lleidahack celebra la 7a edició de la HackEPS, la hackató més gran de les terres de ponent."
           image={hack}
         />
 
-        <div className="max-w-[224px] max-h-[128px] h-[128px] w-[224px]">
-          <div className=" ml-4 mr-4">
+        <div className="adjust max-h-[8rem] h-[8rem] w-[14rem]">
+          <div className="ml-4 mr-4">
             <EventCard_four
               image={background}
               title="Event Title"
             ></EventCard_four>
           </div>
-          <div className="ml-4 mt-[16px] mr-4">
+          <div className="ml-4 mt-[1rem] mr-4">
             <EventCard_four
               image={background}
               title="Event Title"
             ></EventCard_four>
           </div>
-          <div className="ml-[16px] mt-[16px] mr-[16px]">
+          <div className="ml-[1rem] mt-[1rem] mr-[1rem]">
             <EventCard_four
               image={background}
               title="Event Title"
             ></EventCard_four>
           </div>
-          <p className="text-[#ff742f] font-space-mono font-normal tracking-[-0.02em] text-[16px] mt-[5px] leading-[23.7px] ml-[16px]">
+          <p className="text-[#ff742f] font-space-mono font-normal tracking-[-0.02em] text-[1rem] mt-[0.313rem] leading-[1.481rem] ml-[1rem]">
             Veure més...
           </p>
         </div>
