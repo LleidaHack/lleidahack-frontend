75018d86ed4a7a5b1d066ac8981a5deccefb5da5
Quiracle <jsalattorres@gmail.com>
2024-07-19 16:51:59 +0200

    fix (hero landing): brightness background adjusted

--- src/components/landing/HeroSectionHeader/HeroSectionHeader.js
+++ src/components/landing/HeroSectionHeader/HeroSectionHeader.js
@@ -5,8 +5,8 @@ import image from "src/imgs/hackers_group.jpg"
 
 const HeroSectionHeader = () => {
   return (
-    <div className="hero-container flex flex-col w-full bg-background-hero brightness-50 static min-h-0 bg-cover bg-no-repeat bg-center">
-      <div className="hero-section-container z-20 pl-12 pr-12">
+    <div className="hero-container flex flex-col w-full bg-background-hero static min-h-0 bg-cover bg-no-repeat bg-center">
+      <div className="brightness-100 bg-black bg-opacity-50 hero-section-container z-20 pl-12 pr-12">
         <div className="logo flex-1 h-20 my-16">
           <img src={logo} alt="logo + text" />
         </div>
