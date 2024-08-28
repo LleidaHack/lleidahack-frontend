151cef5ba7bf7e53cd39dcc6c3063ddec553907d
naimsg <naimsaadigallego@gmail.com>
2024-08-28 12:32:57 +0200

    feat: Resized cards

--- src/components/landing/AboutUsCardContainer/AboutUsCardContainer.js
+++ src/components/landing/AboutUsCardContainer/AboutUsCardContainer.js
@@ -4,11 +4,12 @@ import { getLleidaHackerGroupById } from "src/services/LleidaHackerGroupService"
 
 const juntaMembers = [getLleidaHackerGroupById(1)];
 const CapsEquips = [getLleidaHackerGroupById(2)];
-/* const Developers = [getLleidaHackerGroupById(3)];
 const Contactes = [getLleidaHackerGroupById(4)];
+const Developers = [getLleidaHackerGroupById(3)];
 const Marketing = [getLleidaHackerGroupById(5)];
-const Techmeeting = [getLleidaHackerGroupById(6)];
-parlar amb marketing de com ordenar això */
+const Techmeetings = [getLleidaHackerGroupById(6)];
+
+const isSmall = true;
 
 const AboutUsCardContainer = () => {
     return(
@@ -22,8 +23,10 @@ const AboutUsCardContainer = () => {
                     <h1 className="font-bold text-5xl">JUNTA </h1>
                     <div className="flex flex-row flex-wrap gap-4 justify-center mt-10 mb-10">
                         {juntaMembers.map((lleidahacker) => (
-                            <AboutUsLleidaHackerCard key={lleidahacker.name} bgcolor={"primaryLanding"}  image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
+                            <AboutUsLleidaHackerCard key={lleidahacker.id} bgcolor={"primaryLanding"}  image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
                         ))}
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} name="Òscar van de Crommert Rodoreda" github="//github.com/naimsg16" linkedIn="linkedin.com" role="President" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
                     </div>
                 </div>
 
@@ -31,7 +34,41 @@ const AboutUsCardContainer = () => {
                     <h1 className="font-bold text-5xl">CAPS D'EQUIP </h1>
                     <div className="flex flex-row flex-wrap gap-4 justify-center mt-10 mb-10">
                         {CapsEquips.map((lleidahacker) => (
-                            <AboutUsLleidaHackerCard key={lleidahacker.name} bgcolor={"primaryLanding"} image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
+                            <AboutUsLleidaHackerCard key={lleidahacker.id} bgcolor={"primaryLanding"} image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
+                        ))}
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                    </div>
+                </div>
+
+                <div className="membres mt-10">
+                    <h1 className="font-bold text-5xl">MEMBRES </h1>
+                    <div className="flex flex-row flex-wrap gap-2 justify-center mt-10 mb-10 ">
+                    <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} small name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} small name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} small name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} small name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        <AboutUsLleidaHackerCard bgcolor={"primaryLanding"} small name="Naïm Saadi Gallego" github="//github.com/naimsg16" linkedIn="linkedin.com" role="Secretari" />
+                        {Contactes.map((lleidahacker) => (
+                            <AboutUsLleidaHackerCard key={lleidahacker.id} small bgcolor={"primaryLanding"} image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
+                        ))}
+                        {Developers.map((lleidahacker) => (
+                            <AboutUsLleidaHackerCard key={lleidahacker.id} small bgcolor={"primaryLanding"} image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
+                        ))}
+                        {Marketing.map((lleidahacker) => (
+                            <AboutUsLleidaHackerCard key={lleidahacker.id} small bgcolor={"primaryLanding"} image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
+                        ))}
+                        {Techmeetings.map((lleidahacker) => (
+                            <AboutUsLleidaHackerCard key={lleidahacker.id} small bgcolor={"primaryLanding"} image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
+                        ))}
+                        {Techmeetings.map((lleidahacker) => (
+                            <AboutUsLleidaHackerCard key={lleidahacker.id} small bgcolor={"primaryLanding"} image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
+                        ))}
+                        {Techmeetings.map((lleidahacker) => (
+                            <AboutUsLleidaHackerCard key={lleidahacker.id} small bgcolor={"primaryLanding"} image={lleidahacker.image} name={lleidahacker.name} github={lleidahacker.github} linkedIn={lleidahacker.linkedIn} role={lleidahacker.role}/>
                         ))}
                     </div>
                 </div>
--- src/components/landing/AboutUsLleidaHackerCard/AboutUsLleidaHackerCard.js
+++ src/components/landing/AboutUsLleidaHackerCard/AboutUsLleidaHackerCard.js
@@ -2,34 +2,43 @@ import React from "react";
 import img from "src/assets/img/llhker.png";
 
 
-const AboutUsLleidaHackerCard = ({name, image , role, github, linkedIn, bgcolor}) => {
-    image = img
-    github = "//github.com/lleidahack"
-    linkedIn = "//linkedin.com/company/lleidahack"
+const AboutUsLleidaHackerCard = ({name, image = img , role, github, linkedIn, bgcolor, small}) => {
+    let textSize = "2xl"
+    let iconSize = "5xl"
+    let linkedInCircle = "[3rem]"
+    let linkedInIcon = "3xl"
+    
+    if (small) {
+        textSize = "xl"
+        iconSize = "4xl"
+        linkedInCircle = "[2.25rem]"
+        linkedInIcon = "2xl"
+    }
+    
     return(
-        <div className={`flex flex-col items-center justify-center bg-${bgcolor}  rounded-md w-96`} >
-        <div className="flex flex-col items-center justify-center mt-4 rounded-full bg-red-500 w-[13rem] h-[13rem] overflow-hidden">
-            <img src={image} alt={name} className="rounded-full w-full h-full object-cover"/>
-        </div>
-        <div className="flex flex-col justify-center items-center">
-            <h1 className="text-2xl font-bold mt-2 mb-0">{name}</h1>
-            <p className="mt-0">{role}</p>
-        </div>
-        <div className="flex flex-row items-center justify-between w-5/12 mb-10">
-            <div>
-                <a href={github} className="text-black text-5xl">
-                    <i class="fa-brands fa-github"></i>
-                </a>
+        <div className={`flex flex-col items-center justify-center bg-${bgcolor} rounded-lg w-1/5 scale-90`} >
+            <div className="flex flex-col items-center justify-center mt-4 rounded-full bg-red-500 w-1/2 h-1/2 overflow-hidden">
+                <img src={image} alt={name} className={`rounded-full w-full h-full object-cover`}/>
             </div>
-            <div>
-                <a href={linkedIn} className="text-primaryLanding text-3xl ">
-                    <div className="bg-black w-[3rem] h-[3rem] rounded-full content-center">
-                        <i class="fa-brands fa-linkedin-in"></i>
-                    </div>
-                </a>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className={`text-${textSize} text-wrap font-bold mt-2 mb-0`}>{name}</h1>
+                <p className="mt-0">{role}</p>
+            </div>
+            <div className="flex flex-row items-center justify-between w-5/12 mb-10">
+                <div>
+                    <a href={github} className={`text-black text-${iconSize}`}>
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                </div>
+                <div>
+                    <a href={linkedIn} className={`text-primaryLanding text-${linkedInIcon} `}>
+                        <div className={`bg-black w-${linkedInCircle} h-${linkedInCircle} rounded-full content-center`}>
+                            <i class="fa-brands fa-linkedin-in"></i>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
     )
 };
 
