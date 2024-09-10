c20372833b350071525b1045845ab99c3fcd4c34
Big_Lolo <95545807+Big-Lolo@users.noreply.github.com>
2024-09-10 17:02:21 +0200

    Update Navbar.js

--- src/components/landing/Navbar/Navbar.js
+++ src/components/landing/Navbar/Navbar.js
@@ -66,7 +66,7 @@ const Navbar = () => {
     <div className="flex flex-col">
       <div className="bg-primaryLanding w-full h-16 px-4 py-2 items-center">
         <div
-          className="md:hidden flex flex-row justify-between mt-3"
+          className="md:hidden flex flex-row justify-between mt-0"
           id="header-points"
         >
           <div className="">
