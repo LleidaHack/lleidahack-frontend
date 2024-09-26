86ec8a5b630aae02e29154acf5a6970e0c8ce257
Big-Lolo <95545807+Big-Lolo@users.noreply.github.com>
2024-09-26 19:50:18 +0200

    Added more layers
    Separated background to layers to play with z-index and animations

--- src/components/hackeps/WaitingScreen/WaitingComponent.js
+++ src/components/hackeps/WaitingScreen/WaitingComponent.js
@@ -2,6 +2,12 @@ import {React, useEffect, useRef} from "react";
 import hackLogo from "src/icons/banner_home_icon.png";
 import "./Waiting.css";
 import Button from "src/components/buttons/Button";
+import fulla2 from "src/assets/2.png";
+import fulla1 from "src/assets/1.png";
+import fulla3 from "src/assets/3.png";
+import fulla4 from "src/assets/4.png";
+import fulla7 from "src/assets/7.png";
+import fulla8 from "src/assets/8.png";
 const WaitingComponent = () => {
 
     const logoRef = useRef(null);
@@ -33,45 +39,62 @@ const WaitingComponent = () => {
     }
 
     return (
-        <div className="backgrounder h-screen w-screen ">
-            {/* <div className="absolute top-48 w-screen h-[50rem] bg-red-400 z-20">
+        <div className=" bg-primaryHackeps h-screen w-screen overflow-hidden">
+            <div className="bg-background-hojas bg-no-repeat bg-cover">
+                <div className="absolute top-48 w-screen h-36 z-20 overflow-hidden	max-w-screen">
+                    <div className="overflow-hidden">
+                        <div class="bird-container bird-container--one">
+                            <div class="bird bird--one"></div>
+                        </div>
+                        <div class="bird-container bird-container--two">
+                            <div class="bird bird--two"></div>
+                        </div>
+                        
+                        <div class="bird-container bird-container--three">
+                            <div class="bird bird--three"></div>
+                        </div>
+                        
+                        <div class="bird-container bird-container--four">
+                            <div class="bird bird--four"></div>
+                        </div>
+                    </div>
                 
-                <div className="bird-container bird-container--one">
-                    <div className="bird bird--one"></div>
                 </div>
-	
-                <div className="bird-container bird-container--two">
-                    <div className="bird bird--two"></div>
-                </div>
-                
-                <div className="bird-container bird-container--three">
-                    <div className="bird bird--three"></div>
-                </div>
-                
-                <div className="bird-container bird-container--four">
-                    <div className="bird bird--four"></div>
-                </div>
-            </div> */}
-            <div className="flex flex-row">
-                <div id="animals-1 " className="w-full">
-
-                </div>
-                <div className="flex flex-col w-screen h-screen text-white text-2xl z-30">
+                <div className="flex flex-row">
+                    <div id="animals-1 " className="w-full">
+                    </div>
+                    <div className="flex flex-col w-screen h-screen text-white text-2xl z-30">
+                        
+                        <div className="loader text-center">
+                            <img className="imagelogo h-[30rem] logo" src={hackLogo} alt="" ref={logoRef}/>
+                        </div>
+                        <div className="mt-32 ml-5 w-full text-center ">
+                            <Button onClick={mikoShow} primary outline>
+                                Inscripcions HackEPS 2024
+                            </Button>                    
+                        </div>
+                    </div>
+                    <div id="animals-2" className="w-full ">
+                    </div>
                     
-                    <div className="loader text-center">
-                        <img className="imagelogo h-[30rem] logo" src={hackLogo} alt="" ref={logoRef}/>
-                    </div>
-                    <div className="mt-32 ml-5 w-full text-center ">
-                        <Button onClick={mikoShow} primary outline>
-                            Inscripcions HackEPS 2024
-                        </Button>                    
+                </div>
+                <div className="absolute bottom-0 w-screen h-36 z-20 overflow-hidden w-screen ">
+                    <div className="flex flex-row w-screen justify-between h-full mx-20">
+                        <div className="bg-red-300 h-full w-20">
+                            <img id="fulla-1" className="fulla-1" src={fulla8} alt=""/>
+                        </div>
+                        <div className="bg-blue-300 h-full w-20">
+                            <img id="fulla-2" className="fulla-2" src={fulla7} alt=""/>
+                        </div>
+                        <div className="bg-green-300 h-full w-20">
+                            <img id="fulla-3" className="fulla-3" src="src/imgs/fulla.png" alt=""/>
+                        </div>
+                        <div className="bg-yellow-300 h-full w-20">
+                            <img id="fulla-4" className="fulla-4" src="src/imgs/fulla.png" alt=""/>
+                        </div>
                     </div>
                 </div>
-                <div id="animals-2" className="w-full">
-
-                </div>
-           </div>
-           
+            </div>
         </div>
     );
 };
