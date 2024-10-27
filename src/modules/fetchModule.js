ab837ecba431edcf7b0f2a9417aa4a405fdf72c1
elver5041 <elver5041@users.noreply.github.com>
2024-10-27 11:17:00 +0000

    Apply formatting changes

--- src/modules/fetchModule.js
+++ src/modules/fetchModule.js
@@ -1,8 +1,12 @@
 function b64EncodeUnicode(str) {
-  return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+  return btoa(
+    encodeURIComponent(str).replace(
+      /%([0-9A-F]{2})/g,
       function toSolidBytes(match, p1) {
-          return String.fromCharCode('0x' + p1);
-  }));
+        return String.fromCharCode("0x" + p1);
+      },
+    ),
+  );
 }
 
 export async function fetchPlus({
