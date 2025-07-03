1a2f674be8027c6d36b4697c2abf28dbcba530ec
EzcXV <enric.zhang@lleidahack.dev>
2025-07-03 23:00:57 +0200

    Cartell bueno
    -Probes 3

--- src/components/hackeps/Sponsors/LogoSponsors.js
+++ src/components/hackeps/Sponsors/LogoSponsors.js
@@ -1,20 +1,17 @@
 import React from "react";
-import "./LogoSponsors.css";
-import wantedFrame1 from "src/assets/Cartell A.png";
 //import wantedFrame2 from "src/assets/Cartell B.png";
 
 const LogoSponsors = ({ image, name }) => {
   return (
-    <div className="relative w-52 h-48 bg-background-cartellA bg-cover items-center justify-center content-center">   
+    <div className="relative w-52 h-48 bg-background-cartellA bg-cover items-center justify-center content-center flex">   
       {/* Contingut centrat dins el marc marró */}
       <div
-        className="
-          w-9/12 h-5/12 flex flex-col items-center justify-center p-4 self-center"
+        className="w-9/12 h-3/12 flex flex-col items-center justify-center mt-8 px-2"
       >
         <img
           src={image}
           alt={name}
-          className="w-full h-auto max-h-[70%] object-contain"
+          className="w-full h-24 object-contain"
         />
       </div>
         {/* <p className="text-xs font-bold text-[#4b2e16] text-center mt-2">
