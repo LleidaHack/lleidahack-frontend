b4125aed56d535f462cded1dad5e170ad4129d20
Big-Lolo <95545807+Big-Lolo@users.noreply.github.com>
2025-07-16 22:24:48 +0200

    Responsive calendar changes

--- src/components/hackeps/Calendar/Calendar.js
+++ src/components/hackeps/Calendar/Calendar.js
@@ -35,7 +35,7 @@ const Calendar = () => {
           return (
             <div
               key={idx}
-              className={`h-10 rounded-md shadow-md flex items-center justify-center ${bgColor} ${textColor}`}
+              className={`md:h-10 h-7 rounded-md shadow-md flex items-center justify-center ${bgColor} ${textColor}`}
             >
               {day || ""}
             </div>
--- src/components/hackeps/Home/Calendar.js
+++ src/components/hackeps/Home/Calendar.js
@@ -14,7 +14,7 @@ const CalendarDates = (props) => {
         Dates
       </TitleGeneralized>
       <Container>
-        <Row>
+        <Row className="flex flex-col md:flex-row gap-4 md:gap-0">
           <Col className="calendar-content">
             <p className="text-justify mr-5">
               Lleidahack et dona la benvinguda a la HackEPS 2024, la primera
