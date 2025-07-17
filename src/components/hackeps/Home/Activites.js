66d2b0f741b3694c09a5681de641f7a4cc74eaf8
DLlol <davlopabril@gmail.com>
2025-07-17 20:29:08 +0200

    Create activities titol and images

--- src/components/hackeps/Home/Activites.js
+++ src/components/hackeps/Home/Activites.js
@@ -1,7 +1,24 @@
 import React from "react";
+import TitleGeneralized from "../TitleGeneralized/TitleGeneralized";
 
 const ActivitiesSection = () => {
-    return <div className="bg-primaryHackeps w-full h-[30rem]">EventsSection</div>;
+    return <div className="bg-secondaryHackeps w-full h-auto">
+        <div className="bg-background-events w-full h-full flex flex-col">
+            <TitleGeneralized padTop="0" underline>
+                Activitats
+            </TitleGeneralized>
+            <img 
+                src="/src/assets/illa.png" 
+                alt="Illa Activitats" 
+                className="w-full h-auto object-cover" 
+            />
+            <img 
+                src="/src/assets/mapa-botella.png" 
+                alt="Botella del tresor" 
+                className="w-full h-auto object-cover"
+            />
+        </div>
+    </div>;
 };
 
 export default ActivitiesSection;
