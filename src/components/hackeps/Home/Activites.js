43e15e4952e41957893c4186668aaf336f766059
Big-Lolo <95545807+Big-Lolo@users.noreply.github.com>
2025-07-22 23:21:08 +0200

    Update Activites.js
    Adelantada la component de activitats

--- src/components/hackeps/Home/Activites.js
+++ src/components/hackeps/Home/Activites.js
@@ -1,24 +1,28 @@
 import React from "react";
 import TitleGeneralized from "../TitleGeneralized/TitleGeneralized";
+import illaMarro from "src/assets/illa.png"
+import ampolla from "src/assets/mapa-botella.png"
 
 const ActivitiesSection = () => {
-    return <div className="bg-secondaryHackeps w-full h-auto">
-        <div className="bg-background-events w-full h-full flex flex-col">
+    return (
+    <div className="bg-secondaryHackeps w-full h-auto px-20 py-10 items-center flex flex-col">
+        <div className="bg-background-events py-16 bg-contain bg-no-repeat w-full bg-center h-[50rem] flex flex-col items-center ">
             <TitleGeneralized padTop="0" underline>
                 Activitats
             </TitleGeneralized>
             <img 
-                src="/src/assets/illa.png" 
+                src={illaMarro} 
                 alt="Illa Activitats" 
-                className="w-full h-auto object-cover" 
+                className=" h-[32rem]" 
             />
             <img 
-                src="/src/assets/mapa-botella.png" 
+                src={ampolla}
                 alt="Botella del tresor" 
-                className="w-full h-auto object-cover"
+                className="h-96 transform translate-y-[-10rem] translate-x-[-23rem] "
             />
         </div>
-    </div>;
+    </div>
+    );
 };
 
 export default ActivitiesSection;
