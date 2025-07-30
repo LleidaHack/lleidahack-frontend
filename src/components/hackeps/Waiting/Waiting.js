9574095e9982624069867e30b19136046a07132e
Big-Lolo <Big-Lolo@users.noreply.github.com>
2025-07-30 15:10:41 +0000

    Apply formatting changes

--- src/components/hackeps/Waiting/Waiting.js
+++ src/components/hackeps/Waiting/Waiting.js
@@ -49,7 +49,6 @@ const Waiting = () => {
         ))}
       </div>
       <div className="capaIsla+Barco flex flex-col md:flex-row justify-between items-center w-full  z-10 h-full ">
-
         <div className="logoHackeps+text flex flex-col  gap-4 order-first md:order-last transform translate-y-0 md:translate-y-0 md:translate-x-[100%] items-center md:items-start  justify-center h-full">
           <img className="w-5/12 md:w-5/12" src={hackLogo} alt="logo hackeps" />
           <p className="text-3xl md:text-5xl font-bold text-primaryHackeps">
@@ -62,7 +61,11 @@ const Waiting = () => {
       </div>
       {/* Infinite moving hours image */}
       <div className="barco absolute bottom-36 md:bottom-36 flex items-end h-full">
-          <img src={barco} className="w-6/12 md:w-3/12 shipMovement" alt="barco" />
+        <img
+          src={barco}
+          className="w-6/12 md:w-3/12 shipMovement"
+          alt="barco"
+        />
       </div>
       <div className="overflow-hidden w-full z-[50] h-28 md:h-32">
         <div className="w-full h-36 absolute bottom-0 bg-repeat-x overflow-hidden wavesSeaAnim animate-olas-scroll transform translate-y-3 md:translate-y-0 bg-contain"></div>
