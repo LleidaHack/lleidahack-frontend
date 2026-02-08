9db6f8cb7070964456b5709640b63a9e7ef344ba
EzcXV <EzcXV@users.noreply.github.com>
2026-02-08 00:13:09 +0000

    Apply formatting changes

--- src/components/navbarComponent/navbarComponent.js
+++ src/components/navbarComponent/navbarComponent.js
@@ -58,9 +58,13 @@ const NavbarComponent = ({
   }, [bgColor]);
 
   return (
-    <nav className={`flex flex-col !sticky top-0 z-50 shadow-md`} style={{ backgroundColor: bgColor2, color: textColor }}>
-      <div 
-        className={`w-full h-16 px-4 py-2 items-center`} style={{ backgroundColor: bgColor2 }} 
+    <nav
+      className={`flex flex-col !sticky top-0 z-50 shadow-md`}
+      style={{ backgroundColor: bgColor2, color: textColor }}
+    >
+      <div
+        className={`w-full h-16 px-4 py-2 items-center`}
+        style={{ backgroundColor: bgColor2 }}
       >
         <div
           className="md:hidden flex flex-row justify-between mt-0"
@@ -82,7 +86,8 @@ const NavbarComponent = ({
             id="container-points"
           >
             <div
-              className={`flex md:justify-between items-center bg-background-patron h-full w-full md:bg-background-none pb-3`} style={{ backgroundColor: bgColor2 }}
+              className={`flex md:justify-between items-center bg-background-patron h-full w-full md:bg-background-none pb-3`}
+              style={{ backgroundColor: bgColor2 }}
             >
               <div className="hidden md:block items-center  ">
                 <div className="flex items-center h-full relative">
